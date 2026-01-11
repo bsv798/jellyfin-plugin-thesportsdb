f823d9c0838eeec72159c8307b5b4ff84c0ba7b1
Sergey Bizin <bizin@listen2u.ru>
2026-01-11 10:02:58 +0700

    Add season provider

---
+++ Jellyfin.Plugin.TheSportsDB/Providers/SeasonImageProvider.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Jellyfin.Plugin.TheSportsDB;
+using Jellyfin.Plugin.TheSportsDB.Providers.IdsExtensions;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Providers;
+using Microsoft.Extensions.Logging;
+using TheSportsDBClient;
+
+namespace Jellyfin.Plugin.TheSportsDB.Providers;
+
+/// <summary>
+/// Season image provider.
+/// </summary>
+public class SeasonImageProvider : IRemoteImageProvider
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<SeasonImageProvider> _logger;
+    private readonly TheSportsDBClientV1 _tsdbClient;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SeasonImageProvider"/> class.
+    /// </summary>
+    /// <param name="httpClientFactory">Instance of the <see cref="IHttpClientFactory"/> interface.</param>
+    /// <param name="logger">Instance of the <see cref="ILogger{SeasonImageProvider}"/> interface.</param>
+    /// <param name="tsdbClient">Instance of <see cref="TheSportsDBClientV1"/>.</param>
+    public SeasonImageProvider(
+        IHttpClientFactory httpClientFactory,
+        ILogger<SeasonImageProvider> logger,
+        TheSportsDBClientV1 tsdbClient)
+    {
+        _httpClientFactory = httpClientFactory;
+        _logger = logger;
+        _tsdbClient = tsdbClient;
+    }
+
+    /// <inheritdoc />
+    public string Name => TheSportsDBPlugin.ProviderName;
+
+    /// <inheritdoc />
+    public bool Supports(BaseItem item)
+    {
+        return item is Season;
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<ImageType> GetSupportedImages(BaseItem item)
+    {
+        yield return ImageType.Primary;
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, CancellationToken cancellationToken)
+    {
+        var season = (Season)item;
+        var series = season.Series;
+
+        if (!series.IsSupported() || season.IndexNumber is null)
+        {
+            return Enumerable.Empty<RemoteImageInfo>();
+        }
+
+        var seasonInfo = await _tsdbClient.GetSeason(series.GetTsdbId(), season.IndexNumber?.ToString(CultureInfo.InvariantCulture) ?? string.Empty).ConfigureAwait(false);
+        var seasonFirst = seasonInfo.Seasons[0];
+        var remoteImages = new List<RemoteImageInfo>
+        {
+            new() { Url = seasonFirst.StrBadge, Type = ImageType.Primary },
+        };
+
+        return remoteImages;
+    }
+
+    /// <inheritdoc />
+    public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken)
+    {
+        return _httpClientFactory.CreateClient(NamedClient.Default).GetAsync(new Uri(url), cancellationToken);
+    }
+}
---
+++ Jellyfin.Plugin.TheSportsDB/Providers/SeasonProvider.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Jellyfin.Plugin.TheSportsDB.Providers.IdsExtensions;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+using Microsoft.Extensions.Logging;
+using TheSportsDBClient;
+
+namespace Jellyfin.Plugin.TheSportsDB.Providers
+{
+    /// <summary>
+    /// Season Provider.
+    /// </summary>
+    public class SeasonProvider : IRemoteMetadataProvider<Season, SeasonInfo>
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<SeasonProvider> _logger;
+        private readonly ILibraryManager _libraryManager;
+        private readonly TheSportsDBClientV1 _tsdbClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SeasonProvider"/> class.
+        /// </summary>
+        /// <param name="httpClientFactory">Instance of the <see cref="IHttpClientFactory"/> interface.</param>
+        /// <param name="logger">Instance of the <see cref="ILogger{SeasonProvider}"/> interface.</param>
+        /// <param name="libraryManager">Instance of <see cref="ILibraryManager"/>.</param>
+        /// <param name="tsdbClient">Instance of <see cref="TheSportsDBClientV1"/>.</param>
+        public SeasonProvider(IHttpClientFactory httpClientFactory, ILogger<SeasonProvider> logger, ILibraryManager libraryManager, TheSportsDBClientV1 tsdbClient)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+            _tsdbClient = tsdbClient;
+            _libraryManager = libraryManager;
+        }
+
+        /// <inheritdoc/>
+        public string Name => TheSportsDBPlugin.ProviderName;
+
+        /// <inheritdoc/>
+        public async Task<MetadataResult<Season>> GetMetadata(SeasonInfo season, CancellationToken cancellationToken)
+        {
+            if (season.IndexNumber == null || !season.SeriesProviderIds.IsSupported())
+            {
+                _logger.LogDebug("No series identity found for {EpisodeName}", season.Name);
+
+                return new MetadataResult<Season>
+                {
+                    QueriedById = true
+                };
+            }
+
+            season.SeriesProviderIds.TryGetValue(TheSportsDBPlugin.ProviderId, out var tsdbid);
+
+            var response = await _tsdbClient.GetSeason(int.Parse(tsdbid ?? "0", CultureInfo.InvariantCulture), season.IndexNumber?.ToString(CultureInfo.InvariantCulture) ?? string.Empty).ConfigureAwait(false);
+
+            return MapSeasonToResult(season, response);
+        }
+
+        private MetadataResult<Season> MapSeasonToResult(SeasonInfo season, SeasonsResponse response)
+        {
+            var seasonFirst = response.Seasons[0];
+            var result = new MetadataResult<Season>
+            {
+                HasMetadata = true,
+                Item = new Season
+                {
+                    IndexNumber = season.IndexNumber,
+                    Overview = seasonFirst.StrPoster,
+                }
+            };
+
+            return result;
+        }
+
+        /// <inheritdoc/>
+        public Task<IEnumerable<RemoteSearchResult>> GetSearchResults(SeasonInfo searchInfo, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Enumerable.Empty<RemoteSearchResult>());
+        }
+
+        /// <inheritdoc/>
+        public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken)
+        {
+            return _httpClientFactory.CreateClient(NamedClient.Default).GetAsync(new Uri(url), cancellationToken);
+        }
+    }
+}
